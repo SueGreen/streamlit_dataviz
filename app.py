c264d8d777b26e9b2457e2299c23375409ad2623
SueGreen <suz.tabaeva@yandex.ru>
2024-08-26 13:19:23 +0300

    Upd zip file loading: via zipfilebuffer

--- app.py
+++ app.py
@@ -15,12 +15,56 @@ st.write('World')
 #    st.success(f"Saved at '{save_dir}'.")
 #    
 
-with open(Path("imgs.zip"), "rb") as file:
-    btn = st.download_button(
-        label="Download data",
-        data=file,
-        file_name=Path("imgs.zip"),
-        #mime="image/jpeg",
-    )
+#with open(Path("imgs.zip"), "rb") as file:
+#    btn = st.download_button(
+#        label="Download data",
+#        data=file,
+#        file_name=Path("imgs.zip"),
+#        #mime="image/jpeg",
+#    )
+
+
+
+import streamlit as st
+import os
+from PIL import Image
+from io import BytesIO
+import requests
+import zipfile
+
+# Sample data
+data = [
+    {"name": "imgs/image_sunset.jpeg"},
+]
+
+## Function to download and display high-quality images
+#def download_image(url):
+#    response = requests.get(url)
+#    img = Image.open(BytesIO(response.content))
+#    return img
+
+# Function to save all images in a zip file
+def save_images():
+    if not os.path.exists("images"):
+        os.makedirs("images")
+
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zf:
+        for idx, row in enumerate(data):
+            #img = download_image(row["image_url"])
+            #img_filename = f"images/{row['name'].replace(' ', '_').lower()}.png"
+            img_filename = Path(row['name'])
+	    img.save(img_filename)
+            zf.write(img_filename, os.path.basename(img_filename))
+
+    zip_buffer.seek(0)
+    return zip_buffer
+
+
+# Button to save all images
+if st.button("Download All Images"):
+    zip_buffer = save_images()
+    st.download_button("Download Zip", data=zip_buffer, file_name="images.zip", mime="application/zip")
+
 
 

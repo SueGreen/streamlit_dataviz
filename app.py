ceb8f694b659a505948a02d745e30e129afbb905
SueGreen <suz.tabaeva@yandex.ru>
2024-08-26 17:05:00 +0300

    Try also saving df

--- app.py
+++ app.py
@@ -9,27 +9,38 @@ st.title('Hello')
 st.write('World')
 
 # Sample data
-data = [
-    {"name": "imgs/sunset.jpeg"},
-    {"name": "imgs/sunrise.jpg"},
-    {"name": "imgs/daylight.jpeg"},
-    {"name": "imgs/night.jpeg"},
-]
-# data = pd.DataFrame.from_dict(data)
+# data = [
+#     {"name": "imgs/sunset.jpeg"},
+#     {"name": "imgs/sunrise.jpg"},
+#     {"name": "imgs/daylight.jpeg"},
+#     {"name": "imgs/night.jpeg"},
+# ]
+data = {
+    "name": [
+        "imgs/sunset.jpeg",
+        "imgs/sunrise.jpg",
+        "imgs/daylight.jpeg",
+        "imgs/night.jpeg",
+    ]
+}
+data = pd.DataFrame.from_dict(data)
 
 
 # Function to save all images in a zip file
 def save_images():
-    Path("images").mkdir(parents=True, exist_ok=True)
+    Path("output").mkdir(parents=True, exist_ok=True)
 
     zip_buffer = BytesIO()
     with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zf:
         for idx, row in enumerate(data):
-            img_filename = Path(row['name'])
-            img = Image.open(img_filename)
-            img.save(img_filename)
-            zf.write(img_filename, img_filename.name)
+            img_filepath = Path(row['name'])
+            img = Image.open(img_filepath)
+            img.save(img_filepath)
+            zf.write(img_filepath, img_filepath.name)
 
+        df_filepath = Path("output") / 'data.csv'
+        data.to_csv(df_filepath)
+        zf.write(df_filepath, df_filepath.name)
     zip_buffer.seek(0)
     return zip_buffer
 
@@ -38,7 +49,7 @@ def save_images():
 zip_buffer = save_images()
 st.write('---')
 st.download_button(
-    "Download All Images (Zip)", 
+    "Download All Data (Zip)", 
     data=zip_buffer, 
     file_name="images.zip", 
     mime="application/zip"
@@ -48,7 +59,7 @@ st.write('---')
 
 for idx in range(len(data)):
     col1, col2 = st.columns([1, 1])
-    img_filename = Path(data[idx].get("name")).name
+    img_filename = Path(data.iloc[idx]["name"]).name
     col1.write(img_filename)
     with col2:
         img_path = Path('imgs') / img_filename

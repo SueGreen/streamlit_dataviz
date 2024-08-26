ec944c75acd10ebf8db3056d9b4b776b361b3a5a
SueGreen <suz.tabaeva@yandex.ru>
2024-08-26 13:05:20 +0300

    Trying st.download_button

--- app.py
+++ app.py
@@ -7,11 +7,20 @@ st.write('World')
 
 
 
-if st.button('Save img', type='primary'):
-    save_dir = Path('Saved')
-    Path(save_dir).mkdir(parents=True, exist_ok=True)
-    img_path = Path('images_sunset.jpeg')
-    shutil.copy(img_path, save_dir)
-    st.success(f"Saved at '{save_dir}'.")
-    
+#if st.button('Save img', type='primary'):
+#    save_dir = Path('Saved')
+#    Path(save_dir).mkdir(parents=True, exist_ok=True)
+#    img_path = Path('images_sunset.jpeg')
+#    shutil.copy(img_path, save_dir)
+#    st.success(f"Saved at '{save_dir}'.")
+#    
+
+with open(Path("images_sunset.jpeg"), "rb") as file:
+    btn = st.download_button(
+        label="Download image",
+        data=file,
+        file_name="images_sunset.jpeg",
+        mime="image/jpeg",
+    )
+
 

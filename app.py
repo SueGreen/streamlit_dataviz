c29faca7a2f8a6e9213e9c6e6737222ef8a21aac
SueGreen <suz.tabaeva@yandex.ru>
2024-08-26 12:44:21 +0300

    Added image loading

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+from pathlib import Path
+
 
 st.title('Hello')
 st.write('World')
+
+
+
+if st.button('Save img', type='primary'):
+    save_dir = Path('Saved')
+    Path(save_dir).mkdir(parents=True, exist_ok=True)
+    img_path = Path('images_sunset.jpeg')
+    shutil.copy(img_path, save_dir)
+    st.success(f"Saved at '{save_dir}'.")
+    
 
